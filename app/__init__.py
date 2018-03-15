5a2e2e42812e38e1ac7d6e39be8e705a36dcaae8
wolfram74 <phaugen@gmail.com>
2018-03-15 12:51:45 -0600

    added database query route

--- app/__init__.py
+++ app/__init__.py
@@ -8,3 +8,35 @@ def load():
     response = make_response(open('./download_test.html').read())
     # data_base_query()
     return response
+
+@app.route('/data')
+def data_return():
+    print(request.args)
+    print("%(year)s-%(month)s-%(day)s" % request.args)
+    print('oh snap')
+    print(type(data_base_query()))
+    # return 'poot'
+    return data_base_query()
+
+def data_base_query():
+    endpoint ="https://omniweb.sci.gsfc.nasa.gov/cgi/nx1.cgi"
+    options = {
+        'activity': 'retrieve',
+        'sars': '4',
+        'res': 'min',
+        'spacecraft':'sc_merge_min',
+        'num_plot':'1',
+        'start_date':'20180207',
+        'end_date':'20180208',
+        'vars':'11',
+        'scale':'Linear',
+        'xstyle':'0',
+        'ystyle':'0',
+        'symbol':'0',
+        'imagex':'640',
+        'imagey':'480'
+    }
+    r = requests.post(endpoint, data=options)
+    # print(r)
+    # print(r.text)
+    return r.text
